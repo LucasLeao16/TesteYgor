7387c8bee03981420484dcbdf68762e91997886c
Lucas Leao <lucas99br@gmail.com>
2023-02-13 19:40:33 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,23 @@
+async function buscarEndereco(cep) {
+  try {
+    const logradouroEL = document.querySelector("#endereco");
+    const bairroEl = document.querySelector("#bairro");
+    const cidadeEl = document.querySelector("#cidade");
+    const ufEl = document.querySelector("#estado");
+    const verificarCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const cepConvertido = await verificarCep.json();
+    if (cepConvertido.erro) {
+      throw Error("CEP não existente !");
+    }
+    logradouroEL.value = cepConvertido.logradouro;
+    bairroEl.value = cepConvertido.bairro;
+    cidadeEl.value = cepConvertido.localidade;
+    ufEl.value = cepConvertido.uf;
+    console.log(cepConvertido);
+    return cepConvertido;
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+const cepEl = document.querySelector("#cep");
+cepEl.addEventListener("focusout", () => buscarEndereco(cepEl.value));
